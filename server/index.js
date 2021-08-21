eff8c05236e782cd63c7bf6b4a135097ddfedba0
emraftis <82290450+emraftis@users.noreply.github.com>
2021-08-21 15:06:20 -0600

    finalized for deployment

--- server/index.js
+++ server/index.js
@@ -10,6 +10,16 @@ app.use(cors());
 const DisplayPage = require("./routes/getAPIs.js");
 app.use("/", DisplayPage);
 
+//production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/public"));
+}
+
+//SPA
+app.get(/.*/, (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 //Server listen
 const port = 3000;
 app.listen(port, () => console.log(`Server on ${port}`));
