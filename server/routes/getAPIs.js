ba43b1e81d7b791840deb099da2147bda57f9ac3
Evan Raftis <evanraftis@gmail.com>
2021-08-21 17:00:02 -0600

    added env variables

--- server/routes/getAPIs.js
+++ server/routes/getAPIs.js
@@ -12,8 +12,7 @@ router.get("/api/top-skills", async (req, res) => {
 
 //function to load top industry skills.
 async function loadIndustrySkills(industry) {
-  const url =
-    "mongodb+srv://user1:gottago@cluster0.vlkxw.mongodb.net/myFirstDatabase";
+  const url = process.env.DB_URL;
   const client = await mongodb.MongoClient.connect(url, {
     useNewUrlParser: true,
   });
@@ -38,13 +37,6 @@ async function loadIndustrySkills(industry) {
   return skillList;
 }
 
-// //Get the penetration % based on skill
-// router.get("/api/skill-penetration", async (req, res) => {
-//   let industry = req.query.ind; // Example: " ?ind=Banking "
-//   const data = await loadSkillsPenetration(industry.toString());
-//   res.send(data);
-// });
-
 //API route for industry growth data
 router.get("/api/ind-growth", async (req, res) => {
   let country = req.query.country; // Example: " ?ind=Banking "
@@ -55,8 +47,7 @@ router.get("/api/ind-growth", async (req, res) => {
 
 //return growth rate data from MongoDB (single object)
 async function loadGrowthRates(country, industry) {
-  const url =
-    "mongodb+srv://user1:gottago@cluster0.vlkxw.mongodb.net/myFirstDatabase";
+  const url = process.env.DB_URL;
   const client = await mongodb.MongoClient.connect(url, {
     useNewUrlParser: true,
   });
@@ -68,60 +59,6 @@ async function loadGrowthRates(country, industry) {
     .toArray();
 
   return object[0];
-}
-
-router.get("/api/industry-name-list", async (req, res) => {
-  const data = await loadIndustryNameList();
-  res.send(data);
-});
-
-async function loadIndustryNameList() {
-  const url =
-    "mongodb+srv://user1:gottago@cluster0.vlkxw.mongodb.net/myFirstDatabase";
-  const client = await mongodb.MongoClient.connect(url, {
-    useNewUrlParser: true,
-  });
-
-  const list = await client
-    .db("myFirstDatabase")
-    .collection("industryskillneeds")
-    .find({})
-    .toArray();
-
-  const uniqueList = [];
-  for (let i = 0; i < list.length; i++) {
-    if (!uniqueList.includes(list[i].industry_name)) {
-      uniqueList.push(list[i].industry_name);
-    }
-  }
-  return uniqueList;
-}
-
-router.get("/api/country-name-list", async (req, res) => {
-  const data = await loadCountryNameList();
-  res.send(data);
-});
-
-async function loadCountryNameList() {
-  const url =
-    "mongodb+srv://user1:gottago@cluster0.vlkxw.mongodb.net/myFirstDatabase";
-  const client = await mongodb.MongoClient.connect(url, {
-    useNewUrlParser: true,
-  });
-
-  const list = await client
-    .db("myFirstDatabase")
-    .collection("industrygrowthrates")
-    .find({})
-    .toArray();
-
-  const uniqueList = [];
-  for (let i = 0; i < list.length; i++) {
-    if (!uniqueList.includes(list[i].country_name)) {
-      uniqueList.push(list[i].country_name);
-    }
-  }
-  return uniqueList;
 }
 
 module.exports = router;
