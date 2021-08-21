1cad4fb476c732becc9ff8e82a21412c2dbbc6df
Evan Raftis <evanraftis@gmail.com>
2021-08-21 15:50:46 -0600

    fixed input

--- server/index.js
+++ server/index.js
@@ -21,5 +21,5 @@ app.get(/.*/, (req, res) => {
 });
 
 //Server listen
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server on ${port}`));
